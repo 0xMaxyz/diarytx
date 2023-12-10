f14f8872dd13c85b6b7cbf7ccac673845beb01b4
Hamid <hamidradpj@gmail.com>
2023-12-10 22:10:04 +0330

    Web removed

--- web/context/constants.js
+++
@@ -1,4 +0,0 @@
-import diaryJson from "../../artifacts/contracts/src/Diary.sol/Diary.json";
-
-export const diaryAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const diaryAbi = diaryJson.abi;
